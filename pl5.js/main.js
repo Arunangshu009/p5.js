6d3b2aa404cf38b6224f7988b400edc319301c34
Arunangshu009 <83114829+Arunangshu009@users.noreply.github.com>
2021-10-04 18:33:05 +0530

    Add files via upload

---
+++ pl5.js/main.js
@@ -0,0 +1,9 @@
+function preload(){
+    img = loadImage("https://c.tenor.com/b7c_4SeFbnYAAAAM/google-gif-spin.gif");
+}
+
+function setup(){
+    canvas.position(110, 250);
+    image=createCapture(image);
+    image.hide();
+}
